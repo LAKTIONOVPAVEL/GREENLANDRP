b119028e85ca0dcfc1d184b9c4eb6c569a1d66d0
LAKTIONOVPAVEL <85648737+LAKTIONOVPAVEL@users.noreply.github.com>
2021-06-10 00:14:27 +0300

    LAND

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+
+    $('.burger-btn').on('click',function(){
+        $('.header-nav').toggleClass('header-nav--open')
+    });
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+      });
+    }
+
+
+
+
+
+
+
+});
